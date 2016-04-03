a1a20745f92427f7229bc84f889641b814a31118
Evan Turner <evturn@gmail.com>
2016-04-03 03:07:43 -0400

    Remove global function to load JSONP

--- dist/bundle.js
+++ dist/bundle.js
@@ -3,8 +3,11 @@
 
   console.log('me like the way you work it. no diggity.')
 
+  var map = L.map('map').setView([33.858631, -118.279602], 7);
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+
   const quakes = Rx.DOM.jsonpRequest({
-    url: QUAKE_URL,
+    url: 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/' + 'summary/all_day.geojsonp',
     jsonpCallback: 'eqfeed_callback'
   })
   .flatMap(result => Rx.Observable.from(result.response.features))
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,11 @@
 import app from './app';
 
+const map = L.map('map').setView([33.858631, -118.279602], 7);
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+
 const quakes = Rx.DOM.jsonpRequest({
-  url: QUAKE_URL,
+  url: 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/' + 'summary/all_day.geojsonp',
   jsonpCallback: 'eqfeed_callback'
 })
 .flatMap(result => Rx.Observable.from(result.response.features))
