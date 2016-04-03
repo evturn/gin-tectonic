fa503e4a7db80eb8168762d7fac83bf475ebe6f7
Evan Turner <evturn@gmail.com>
2016-04-03 03:17:47 -0400

    Emit an incrementing number every five seconds

--- src/js/index.js
+++ src/js/index.js
@@ -4,16 +4,24 @@ const map = L.map('map').setView([33.858631, -118.279602], 7);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
 
-const quakes = Rx.DOM.jsonpRequest({
+const req = {
   url: 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/' + 'summary/all_day.geojsonp',
   jsonpCallback: 'eqfeed_callback'
-})
-.flatMap(result => Rx.Observable.from(result.response.features))
-.map(quake => {
+};
+
+const quakes = Rx.Observable
+  .interval(5000)
+  .flatMap(() =>
+    Rx.DOM.jsonpRequest(req).retry(3)
+  )
+  .flatMap(result =>
+    Rx.Observable.from(result.response.features)
+  )
+  .distinct(quake => quake.properties.code);
+
+  quakes.subscribe(quake => {
   const [ lng, lat ] = quake.geometry.coordinates;
   const size = quake.properties.mag * 10000;
 
-  return { lat, lng, size };
+  L.circle([ lat, lng ], size).addTo(map)
 });
-
-quakes.subscribe(quake => L.circle([ quake.lat, quake.lng ], quake.size).addTo(map));
